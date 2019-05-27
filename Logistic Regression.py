bf21ee3f2cf0d119ddec475d99e88ad4f6b865e8
mitchx <jmitchward@gmail.com>
2019-05-27 15:11:08 -0400

    Class structure & inheritance introduced.

--- Logistic Regression.py
+++ Logistic Regression.py
@@ -5,57 +5,55 @@ from math import exp
 import basic_func
 
 
-def gradient_descent(data, learn, iterations):
+class logistic_regression(basic_func):
     # Returns list of 41 zeros
     weights = [0.0] * 41
-    # For the pre-determined number of iterations
-    for eachIter in range(iterations):
-        sumError = 0
-        for row in range(len(data)):
-            print("Updating Weights {:3.2%}".format(row / (len(data))), end="\r")
-            row = data.iloc[row]
-            result = predict(row, weights)
-            error = (result - row[41])
-            sumError += (.5) * (error ** 2)
-            weights[0] = weights[0] - learn * (1 / (len(data))) * error * result
-            for i in range(40):
-                weights[i + 1] = weights[i + 1] - learn * (1 / (len(data))) * error * row[i]
-    return weights
 
+    def gradient_descent(self, data, learn, iterations):
 
-def predict(row, weights):
-    # Store the initial weight
-    weight = weights[0]
-    for each in range(40):
-        # Using the logistic function, calculate the predicted output
-        weight += (weights[each + 1] * row[each])
-    if weight < 0:
-        return 1.0 - 1 / (1.0 + exp(weight))
-    else:
-        return 1.0 / (1.0 + exp(-weight))
+        # For the pre-determined number of iterations
+        for eachIter in range(iterations):
+            sumError = 0
+            for row in range(len(data)):
+                print("Updating Weights {:3.2%}".format(row / (len(data))), end="\r")
+                row = data.iloc[row]
+                result = data.predict(row, self.weights)
+                error = (result - row[41])
+                sumError += (.5) * (error ** 2)
+                self.weights[0] = self.weights[0] - learn * (1 / (len(data))) * error * result
+                for i in range(40):
+                    self.weights[i + 1] = self.weights[i + 1] - learn * (1 / (len(data))) * error * row[i]
+        return self.weights
 
-
-def main(data, testData):
-    learningRate = 0.2
-    iterations = 10
-    # Returns the list of weights
-    weights = gradient_descent(data, learningRate, iterations)
-    predictions = list()
-    for row in range(len(testData)):
-        print("Predicting {:3.2%}".format(row / (len(testData))), end="\r")
-        row = testData.iloc[row]
-        prediction = predict(row, weights)
-        # Rounds prediction result to 2 decimal places.
-        if prediction > 0.5:
-            predictions.append(1)
+    def predict(self, row):
+        # Store the initial weight
+        weight = self.weights[0]
+        for each in range(40):
+            # Using the logistic function, calculate the predicted output
+            weight += (self.weights[each + 1] * row[each])
+        if weight < 0:
+            return 1.0 - 1 / (1.0 + exp(weight))
         else:
-            predictions.append(0)
-    basic_func.log_reg_accuracy(predictions, testData)
+            return 1.0 / (1.0 + exp(-weight))
 
+    def main(self, data, testData):
+        learningRate = 0.2
+        iterations = 10
+        # Returns the list of weights
+        self.weights = data.gradient_descent(data, learningRate, iterations)
+        predictions = list()
+        for row in range(len(testData)):
+            print("Predicting {:3.2%}".format(row / (len(testData))), end="\r")
+            row = testData.iloc[row]
+            prediction = data.predict(row, self.weights)
+            # Rounds prediction result to 2 decimal places.
+            if prediction > 0.5:
+                predictions.append(1)
+            else:
+                predictions.append(0)
+        basic_func.log_reg_accuracy(predictions, testData)
 
-trainSet = basic_func.encoded_training_data
-testSet = basic_func.encoded_test_data
-# Limits the dataset to the first 50k entries. Used to save time.
 
 print('Beginning Logistic Regression algorithm.')
-main(trainSet, testSet)
+log_reg = logistic_regression()
+log_reg.main(basic_func.encoded_training_data, basic_func.encoded_test_data)
--- basic_func.py
+++ basic_func.py
@@ -5,90 +5,103 @@ from math import sqrt
 from math import pi
 from math import exp
 
-trainData = pd.read_csv(
-    'http://archive.ics.uci.edu/ml/machine-learning-databases/census-income-mld/census-income.test.gz', header=None)
-testData = pd.read_csv(
-    'https://archive.ics.uci.edu/ml/machine-learning-databases/census-income-mld/census-income.data.gz', header=None)
 
+# NOTE: Is it faster to put testing data and training data in two seperate class instances to eliminate passing
+# the dataset being used in a function every time? The weights for LR would have to be passed between after the test
+# data is completed, the NB percentages the same. How many functions would I need to rewrite to accommodate? Later.
+
+# NOTE: Should these be static? Does their function depend on the class instance?
 
 # Import the data from the UCI repository, assign it to separate variables
+class basic_func:
+    # Declaring here to pull out any specificity for future use
+    # These declarations must ONLY be non dependent on class function call
 
-def encode_values(dataset, column):
-    dataset[column] = dataset[column].astype('category')
-    dataset[column] = dataset[column].cat.codes
-    dataset[column] = dataset[column].astype('int')
-
-
-def format_data(dataset):
     # 41 total features of the data set, 31 require encoding
     numFeatures = [0, 2, 3, 5, 16, 17, 18, 24, 30, 39]
     catFeatures = [1, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 19, 20, 21, 22, 23, 25, 26, 27, 28, 29, 31, 32, 33,
                    34, 35, 36, 37, 38, 40]
     questionFeatures = [25, 26, 27, 29]
 
-    print('Encoding categorical features.')
-    for each in catFeatures:
-        encode_values(dataset, each)
-        # Encode class value.
-        # Changes the actual value to a numerical value, allowing it to be used in the algorithm
+    train_data = pd.read_csv(
+        'http://archive.ics.uci.edu/ml/machine-learning-databases/census-income-mld/census-income.test.gz', header=None)
+    test_data = pd.read_csv(
+        'https://archive.ics.uci.edu/ml/machine-learning-databases/census-income-mld/census-income.data.gz',
+        header=None)
 
-    encode_values(dataset, 41)
-    for value in questionFeatures:
-        dataset[value].replace(' ?', dataset.describe(include='all')[value][2], inplace=True)
-    # Take the categorical features, feed them to the encode function.
-    print('Encoding numerical features.')
-    for each in numFeatures:
-        mean, std = dataset[each].mean(), dataset[each].std()
-        dataset.iloc[:, each] = (dataset[each] - mean) / std
-    # Standardize categorical features
-    print('Standardizing categorical features.')
-    for each in catFeatures:
-        dataset.loc[:, each] = (dataset[each] - dataset[each].mean()) / dataset[each].std()
-    return dataset
+    def __init__(self):
+        # Request dataframes when class is initialized for future use
 
+        # Blanket formatting for all algorithms
+        print("Formatting the datasets.")
+        encoded_training_data = self.format_data(self.train_data)
+        encoded_test_data = self.format_data(self.test_data)
 
-# Calculates probability
-def probability(value, mean, sdev):
-    condProb = exp(-((value - mean) ** 2 / (2 * sdev ** 2)))
-    return (1 / (sqrt(2 * pi) * sdev)) * condProb
+    @staticmethod
+    def encode_values(dataset, column):
+        dataset[column] = dataset[column].astype('category')
+        dataset[column] = dataset[column].cat.codes
+        dataset[column] = dataset[column].astype('int')
 
+    def format_data(self, dataset):
 
-# Calculates the mean and standard deviation for each column
-def basic_calc(dataset):
-    summaries = list()
+        print('Encoding categorical features.')
+        for each in self.catFeatures:
+            dataset.encode_values(dataset, each)
+            # Encode class value.
+            # Changes the actual value to a numerical value, allowing it to be used in the algorithm
 
-    # for each in hiImpact:
-    for each in range(41):
-        mean = dataset[each].mean()
-        sdev = dataset[each].std()
-        # creates a list containing the mean and s-dev for each feature in the set.
-        summaries.append([mean, sdev])
-    return summaries
+        dataset.encode_values(dataset, 41)
+        for value in self.questionFeatures:
+            dataset[value].replace(' ?', dataset.describe(include='all')[value][2], inplace=True)
+        # Take the categorical features, feed them to the encode function.
+        print('Encoding numerical features.')
+        for each in self.numFeatures:
+            mean, std = dataset[each].mean(), dataset[each].std()
+            dataset.iloc[:, each] = (dataset[each] - mean) / std
+        # Standardize categorical features
+        print('Standardizing categorical features.')
+        for each in self.catFeatures:
+            dataset.loc[:, each] = (dataset[each] - dataset[each].mean()) / dataset[each].std()
+        return dataset
 
+    # Calculates probability
+    @staticmethod
+    def probability(value, mean, sdev):
+        condProb = exp(-((value - mean) ** 2 / (2 * sdev ** 2)))
+        return (1 / (sqrt(2 * pi) * sdev)) * condProb
 
-def log_reg_accuracy(predict, data):
-    correct = 0
-    print('')
-    dSize = len(data)
-    for i in range(dSize):
-        print("Scoring {:3.2%}".format(i / (len(data))), end="\r")
-        if data.iloc[i][41] == predict[i]:
-            correct += 1
-    print('')
-    print('Accuracy:', round(((correct / dSize) * 100.0)), '%')
+    # Calculates the mean and standard deviation for each column
+    @staticmethod
+    def basic_calc(dataset):
+        summaries = list()
 
+        # for each in hiImpact:
+        for each in range(41):
+            mean = dataset[each].mean()
+            sdev = dataset[each].std()
+            # creates a list containing the mean and s-dev for each feature in the set.
+            summaries.append([mean, sdev])
+        return summaries
 
-def accuracy(predict, data):
-    print('Beginning accuracy rating.')
-    correct = 0
-    dSize = len(data)
-    for i in range(dSize):
-        if data.iloc[i][41] == predict[i]:
-            correct += 1
-    print('Accuracy:', round(((correct / dSize) * 100.0)), '%')
+    @staticmethod
+    def log_reg_accuracy(predict, data):
+        correct = 0
+        print('')
+        dSize = len(data)
+        for i in range(dSize):
+            print("Scoring {:3.2%}".format(i / (len(data))), end="\r")
+            if data.iloc[i][41] == predict[i]:
+                correct += 1
+        print('')
+        print('Accuracy:', round(((correct / dSize) * 100.0)), '%')
 
-
-# Blanket formatting for all algorithms
-print("Formatting the datasets.")
-encoded_training_data = format_data(trainData)
-encoded_test_data = format_data(testData)
+    @staticmethod
+    def accuracy(predict, data):
+        print('Beginning accuracy rating.')
+        correct = 0
+        dSize = len(data)
+        for i in range(dSize):
+            if data.iloc[i][41] == predict[i]:
+                correct += 1
+        print('Accuracy:', round(((correct / dSize) * 100.0)), '%')
