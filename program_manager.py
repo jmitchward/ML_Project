b72a31e8b4285553b43b8e583495da62ec204b6f
Mitch <jmitchward@gmail.com>
2020-04-30 15:32:32 -0400

    New Database

--- program_manager.py
+++ program_manager.py
@@ -28,16 +28,16 @@ class menu(database_manager.df_manage):
         # Misc Values
         self.skip_check = "no"
 
-        self.ml_instance = './ml_data/ml_instance'
-        self.nb_path = './ml_data/nb_instance'
-        self.lr_path = './ml_data/lr_instance'
-        self.dt_path = './ml_data/dt_instance'
+        self.ml_instance = './covid_data/ml_instance'
+        self.nb_path = './covid_data/nb_instance'
+        self.lr_path = './covid_data/lr_instance'
+        self.dt_path = './covid_data/dt_instance'
 
-        #self.train_data = pd.read_csv('divorce.csv', header=None)
-        self.train_data = pd.read_csv('./ml_data/census_income_real.data', header=None)
+        self.train_data = pd.read_pickle('./covid_data/new_dataset.pkl')
+        #self.train_data = pd.read_csv('./ml_data/census_income_real.data', header=None)
         # http://archive.ics.uci.edu/ml/machine-learning-databases/census-income-mld/census-income.test.gz
-        #self.test_data = pd.read_csv('divorce.csv', header=None)
-        self.test_data = pd.read_csv('./ml_data/census_income_test.test', header=None)
+        self.test_data = pd.read_pickle('./covid_data/new_dataset.pkl')
+        #self.test_data = pd.read_csv('./ml_data/census_income_test.test', header=None)
         # https://archive.ics.uci.edu/ml/machine-learning-databases/census-income-mld/census-income.data.gz
         self.data = self.train_data
         # START
